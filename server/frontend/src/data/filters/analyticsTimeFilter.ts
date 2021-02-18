0f89ee49d55df36daf84d65bf2b8e57951930ea0
werzl <adam@houseofhewitt.co.uk>
2021-02-18 14:57:04 +0000

    Update analyticsTimeFilter.ts

--- server/frontend/src/data/filters/analyticsTimeFilter.ts
+++ server/frontend/src/data/filters/analyticsTimeFilter.ts
@@ -4,13 +4,13 @@ const now = moment();
 const then = moment().subtract(1, "hour");
 
 export interface AnalyticsTimeFilter {
-    timestampRangeStart: moment.Moment,
-    timestampRangeEnd: moment.Moment
+	timestampRangeStart: moment.Moment,
+	timestampRangeEnd: moment.Moment
 }
 
-const analyticsTimeFilter : AnalyticsTimeFilter = {
-    timestampRangeStart: then,
-    timestampRangeEnd: now,
-}
+const analyticsTimeFilter: AnalyticsTimeFilter = {
+	timestampRangeStart: then,
+	timestampRangeEnd: now,
+};
 
 export default analyticsTimeFilter;
