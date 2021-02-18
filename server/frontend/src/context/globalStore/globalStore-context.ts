6e8788191a4bac4dc1d1fbf694099e7f892fbf81
werzl <adam@houseofhewitt.co.uk>
2021-02-18 15:09:52 +0000

    Updated Filters.tsx

--- server/frontend/src/context/globalStore/globalStore-context.ts
+++ server/frontend/src/context/globalStore/globalStore-context.ts
@@ -13,7 +13,41 @@ export interface GlobalStoreContextProps {
 	updateAnalyticsTimeFilter: (timeDateFilter: any) => void,
 	selectedFilters: any[],
 	navExpanded: boolean,
-	toggleNavExpanded: () => void
+	toggleNavExpanded: () => void,
+	addFilterInput: (filter: any) => void,
+	fileFilter: [
+		{
+			id: string,
+			filterName: string,
+			checkboxList: [
+				{
+					id: string,
+					type: string,
+					title: string,
+					isChecked: boolean,
+					fileTypeEnum: number
+				}
+			]
+		}
+	],
+	riskFilter: [
+		{
+			id: "risk",
+			filterName: "Risk",
+			checkboxList: [
+				{
+					id: string,
+					title: string,
+					titleColor: string,
+					isChecked: boolean,
+					type: string,
+					riskEnum: number
+				}
+			]
+		}
+	]
+	removeFilter: (filter: any) => void,
+	updateRequestHistoryTimeFilter: (timeDateFilter: any) => void,
 }
 
 export const GlobalStoreContext = createContext<Partial<GlobalStoreContextProps>>({});
