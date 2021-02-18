a9d027c7ed97dd0426f700b74a5f673a956e90a2
werzl <adam@houseofhewitt.co.uk>
2021-02-18 13:46:54 +0000

    Update index.ts

--- server/frontend/src/context/globalStore/api/index.ts
+++ server/frontend/src/context/globalStore/api/index.ts
@@ -3,17 +3,17 @@ import Routes from "../../../Routes";
 const routes = new Routes().root;
 
 export const getVersion = async (): Promise<string> => {
-    const response = await fetch(routes.versionRoute, {
-        method: "GET",
-        headers: {
-            "Accept": "*/*",
-            "Content-Type": "application/json"
-        }
-    });
+	const response = await fetch(routes.versionRoute, {
+		method: "GET",
+		headers: {
+			"Accept": "*/*",
+			"Content-Type": "application/json"
+		}
+	});
 
-    if (!response.ok) {
-        throw response.statusText;
-    }
+	if (!response.ok) {
+		throw response.statusText;
+	}
 
-    return response.text();
-}
+	return response.text();
+};
