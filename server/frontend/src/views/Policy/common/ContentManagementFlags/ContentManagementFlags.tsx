6f6eb2521ed211a4c9b5765981d713193c7dd4a3
werzl <adam@houseofhewitt.co.uk>
2021-02-18 15:18:11 +0000

    Update ContentManagementFlags.tsx

--- server/frontend/src/views/Policy/common/ContentManagementFlags/ContentManagementFlags.tsx
+++ server/frontend/src/views/Policy/common/ContentManagementFlags/ContentManagementFlags.tsx
@@ -14,88 +14,88 @@ import PdfContentManagementFlags from "./PdfContentManagementFlags/PdfContentMan
 import classes from "./ContentManagementFlags.module.scss";
 
 export interface ContentManagementFlagsProps {
-    contentManagementFlags: ContentFlags,
-    currentPolicyContentManagementFlags: ContentFlags,
-    updateContentFlags?: (newContentFlags: ContentFlags) => void,
-    disabled?: boolean
+	contentManagementFlags: ContentFlags,
+	currentPolicyContentManagementFlags: ContentFlags,
+	updateContentFlags?: (newContentFlags: ContentFlags) => void,
+	disabled?: boolean
 }
 
 const ContentManagementFlags = (props: ContentManagementFlagsProps) => {
-    const { contentManagementFlags, currentPolicyContentManagementFlags, updateContentFlags } = props;
+	const { contentManagementFlags, currentPolicyContentManagementFlags, updateContentFlags } = props;
 
-    const updateWordContentFlags = (newWordContentFlags: WordContentFlags) => {
-        updateContentFlags({
-            ...contentManagementFlags,
-            wordContentManagement: newWordContentFlags
-        });
-    }
+	const updateWordContentFlags = (newWordContentFlags: WordContentFlags) => {
+		updateContentFlags({
+			...contentManagementFlags,
+			wordContentManagement: newWordContentFlags
+		});
+	};
 
-    const updateExcelContentFlags = (newExcelContentFlags: ExcelContentFlags) => {
-        updateContentFlags({
-            ...contentManagementFlags,
-            excelContentManagement: newExcelContentFlags
-        });
-    }
+	const updateExcelContentFlags = (newExcelContentFlags: ExcelContentFlags) => {
+		updateContentFlags({
+			...contentManagementFlags,
+			excelContentManagement: newExcelContentFlags
+		});
+	};
 
-    const updatePowerpointContentFlags = (newPowerpointContentFlags: PowerpointContentFlags) => {
-        updateContentFlags({
-            ...contentManagementFlags,
-            powerPointContentManagement: newPowerpointContentFlags
-        });
-    }
+	const updatePowerpointContentFlags = (newPowerpointContentFlags: PowerpointContentFlags) => {
+		updateContentFlags({
+			...contentManagementFlags,
+			powerPointContentManagement: newPowerpointContentFlags
+		});
+	};
 
-    const updatePdfContentFlags = (newPdfContentFlags: PdfContentFlags) => {
-        updateContentFlags({
-            ...contentManagementFlags,
-            pdfContentManagement: newPdfContentFlags
-        });
-    }
+	const updatePdfContentFlags = (newPdfContentFlags: PdfContentFlags) => {
+		updateContentFlags({
+			...contentManagementFlags,
+			pdfContentManagement: newPdfContentFlags
+		});
+	};
 
-    return (
-        <div className={classes.contentFlagsContainer}>
-            <div className={classes.block}>
-                <h2>Word</h2>
-                <div data-test-id="currentPolicySectionWord" className={classes.CurrentRow}>
-                    <WordContentManagementFlags
-                        initialWordContentFlags={contentManagementFlags.wordContentManagement}
-                        updateWordContentFlags={updateWordContentFlags}
-                        currentWordContentFlags={currentPolicyContentManagementFlags.wordContentManagement}
-                        allDisabled={props.disabled} />
-                </div>
-            </div>
-            <div className={classes.block}>
-                <h2>Excel</h2>
-                <div data-test-id="currentPolicySectionExcel" className={classes.CurrentRow}>
-                    <ExcelContentManagementFlags
-                        initialExcelContentFlags={contentManagementFlags.excelContentManagement}
-                        updateExcelContentFlags={updateExcelContentFlags}
-                        currentExcelContentFlags={currentPolicyContentManagementFlags.excelContentManagement}
-                        allDisabled={props.disabled} />
-                </div>
-            </div>
-            <div className={classes.block}>
-                <h2>Powerpoint</h2>
-                <div data-test-id="currentPolicySectionPowerpoint" className={classes.CurrentRow}>
-                    <PowerpointContentManagementFlags
-                        initialPowerpointContentFlags={contentManagementFlags.powerPointContentManagement}
-                        updatePowerpointContentFlags={updatePowerpointContentFlags}
-                        currentPowerPointContentFlags={currentPolicyContentManagementFlags.powerPointContentManagement}
-                        allDisabled={props.disabled} />
-                </div>
-            </div>
+	return (
+		<div className={classes.contentFlagsContainer}>
+			<div className={classes.block}>
+				<h2>Word</h2>
+				<div data-test-id="currentPolicySectionWord" className={classes.CurrentRow}>
+					<WordContentManagementFlags
+						initialWordContentFlags={contentManagementFlags.wordContentManagement}
+						updateWordContentFlags={updateWordContentFlags}
+						currentWordContentFlags={currentPolicyContentManagementFlags.wordContentManagement}
+						allDisabled={props.disabled} />
+				</div>
+			</div>
+			<div className={classes.block}>
+				<h2>Excel</h2>
+				<div data-test-id="currentPolicySectionExcel" className={classes.CurrentRow}>
+					<ExcelContentManagementFlags
+						initialExcelContentFlags={contentManagementFlags.excelContentManagement}
+						updateExcelContentFlags={updateExcelContentFlags}
+						currentExcelContentFlags={currentPolicyContentManagementFlags.excelContentManagement}
+						allDisabled={props.disabled} />
+				</div>
+			</div>
+			<div className={classes.block}>
+				<h2>Powerpoint</h2>
+				<div data-test-id="currentPolicySectionPowerpoint" className={classes.CurrentRow}>
+					<PowerpointContentManagementFlags
+						initialPowerpointContentFlags={contentManagementFlags.powerPointContentManagement}
+						updatePowerpointContentFlags={updatePowerpointContentFlags}
+						currentPowerPointContentFlags={currentPolicyContentManagementFlags.powerPointContentManagement}
+						allDisabled={props.disabled} />
+				</div>
+			</div>
 
-            <div className={classes.block}>
-                <h2>PDF</h2>
-                <div data-test-id="currentPolicySectionPdf" className={classes.CurrentRow}>
-                    <PdfContentManagementFlags
-                        initialPdfContentFlags={contentManagementFlags.pdfContentManagement}
-                        updatePdfContentFlags={updatePdfContentFlags}
-                        currentPdfContentFlags={currentPolicyContentManagementFlags.pdfContentManagement}
-                        allDisabled={props.disabled} />
-                </div>
-            </div>
-        </div>
-    );
-}
+			<div className={classes.block}>
+				<h2>PDF</h2>
+				<div data-test-id="currentPolicySectionPdf" className={classes.CurrentRow}>
+					<PdfContentManagementFlags
+						initialPdfContentFlags={contentManagementFlags.pdfContentManagement}
+						updatePdfContentFlags={updatePdfContentFlags}
+						currentPdfContentFlags={currentPolicyContentManagementFlags.pdfContentManagement}
+						allDisabled={props.disabled} />
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default ContentManagementFlags;
