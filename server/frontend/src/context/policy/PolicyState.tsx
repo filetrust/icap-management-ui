622ae930eb5e5619088f9b48bb1eb0b92c4bc1e7
werzl <adam@houseofhewitt.co.uk>
2021-02-21 18:01:54 +0000

    add default currentPolicy.ncfsPolicy to PolicyState

--- server/frontend/src/context/policy/PolicyState.tsx
+++ server/frontend/src/context/policy/PolicyState.tsx
@@ -17,6 +17,7 @@ import {
 	getPaginatedPolicyHistory
 } from "./api";
 import PaginationModel from "../../../../src/common/models/PolicyManagementService/PolicyHistory/GetPaginatedPolicyHistoryRequest/PaginationModel/PaginationModel";
+import { NcfsOption } from "../../../../src/common/models/enums/NcfsOption";
 
 interface InitialPolicyState {
 	currentPolicy: Policy,
@@ -109,6 +110,15 @@ export const PolicyState = (props: { children: React.ReactNode }) => {
 		setCurrentPolicy(currentPolicy as Policy);
 
 		_handleNullPolicies(draftPolicy as Policy, PolicyType.Draft);
+
+		// TODO: Remove once Policy Management API doesn't return null for ncfsPolicy.ncfsActions
+		if ((draftPolicy as Policy).ncfsPolicy.ncfsActions === null) {
+			(draftPolicy as Policy).ncfsPolicy.ncfsActions = {
+				unprocessableFileTypeAction: NcfsOption.Relay,
+				glasswallBlockedFilesAction: NcfsOption.Relay
+			};
+		}
+
 		setDraftPolicy(draftPolicy as Policy);
 		setNewDraftPolicy(draftPolicy as Policy);
 
