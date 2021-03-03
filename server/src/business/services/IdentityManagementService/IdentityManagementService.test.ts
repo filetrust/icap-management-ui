3bc5f03f56cbd7b7d378af0357891f0839aa27e3
werzl <adam@houseofhewitt.co.uk>
2021-03-03 00:09:37 +0000

    getUsers

--- server/src/business/services/IdentityManagementService/IdentityManagementService.test.ts
+++ server/src/business/services/IdentityManagementService/IdentityManagementService.test.ts
@@ -4,6 +4,7 @@ import axios, { CancelToken } from "axios";
 
 import NewUser from "../../../common/models/IdentityManagementService/NewUser/NewUser";
 import User from "../../../common/models/IdentityManagementService/User/User";
+import { UserStatus } from "../../../common/models/enums/UserStatus";
 import { AuthenticateRequest, AuthenticateResponse } from "../../../common/models/IdentityManagementService/Authenticate";
 import { NewUserRequest } from "../../../common/models/IdentityManagementService/NewUser";
 import { ForgotPasswordRequest } from "../../../common/models/IdentityManagementService/ForgotPassword/ForgotPasswordRequest";
@@ -202,6 +203,31 @@ describe("IdentityManagementService", () => {
 
 			// Assert
 			expect(result).toEqual(expectedResetPasswordResponse);
+		});
+	});
+
+	describe("getUsers", () => {
+		let getUsersStub: SinonStub;
+
+		const users = [
+			new User("id", "firstName", "lastName", "username", "email@email.com", UserStatus.Active),
+			new User("id2", "firstName", "lastName", "username", "email@email.com", UserStatus.Deactivated)
+		];
+
+		beforeEach(() => {
+			getUsersStub = stub(IdentityManagementApi, "getUsers").resolves(users);
+		});
+
+		afterEach(() => {
+			getUsersStub.restore();
+		});
+
+		it("returns_correct_response", async () => {
+			// Act
+			const result = await identityManagementService.getUsers("url", cancellationToken);
+
+			// Assert
+			expect(result).toEqual(users);
 		});
 	});
 });
