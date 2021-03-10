fdea7b880640b51d5e4b05d6b63512022001a017
werzl <adam@houseofhewitt.co.uk>
2021-03-10 13:43:55 +0000

    SaveUserChangesRequest

--- server/src/common/models/IdentityManagementService/SaveUserChanges/SaveUserChangesRequest.ts
+++ server/src/common/models/IdentityManagementService/SaveUserChanges/SaveUserChangesRequest.ts
@@ -1,3 +1,4 @@
+import { ArgumentNullException } from "../../errors/ArgumentNullException";
 import NewUser from "../NewUser/NewUser";
 import User from "../User/User";
 
@@ -9,11 +10,29 @@ export class SaveUserChangesRequest {
     newUsers: NewUser[];
     deletedUsers: string[];
 
-    constructor(updateUserUrl: string, newUserUrl: string,
-        deleteUserUrl: string, updatedUsers: User[], newUsers: NewUser[], deletedUsers: string[]) {
+    constructor(
+        updateUserUrl: string,
+        newUserUrl: string,
+        deleteUserUrl: string,
+        updatedUsers: User[],
+        newUsers: NewUser[],
+        deletedUsers: string[]) {
+
+        if (!updateUserUrl) {
+            throw new ArgumentNullException("updateUserUrl");
+        }
         this.updateUserUrl = updateUserUrl;
+
+        if (!newUserUrl) {
+            throw new ArgumentNullException("newUserUrl");
+        }
         this.newUserUrl = newUserUrl;
+
+        if (!deleteUserUrl) {
+            throw new ArgumentNullException("deleteUserUrl");
+        }
         this.deleteUserUrl = deleteUserUrl;
+
         this.updatedUsers = updatedUsers;
         this.newUsers = newUsers;
         this.deletedUsers = deletedUsers;
